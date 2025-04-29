0dd8e71167ee11ff298480230ffff5b95e757130
Abdelghani Adel <a.adel@rockettravelsystem.com>
2025-04-29 10:44:13 +0300

    about us

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -5,17 +5,18 @@ import data from "@/public/data/aboutUs.json";
 const Page = () => {
   return (
     <div>
-      <section className="relative w-full h-[80vh]">
+      <section className="relative w-full h-[40vh] 2xl:h-[80vh]">
         <Image src={data.heroBackground} alt="" fill />
       </section>
 
       <section className="bg-secondary p-10 space-y-5 text-white">
-        <div className="flex justify-between items-center">
+        <div className="flex flex-col-reverse 2xl:flex-row justify-between items-center">
           <div className="max-w-2xl text-white">
             <h1 className="text-4xl font-bold mb-5">{data.heroTitle}</h1>
             <div dangerouslySetInnerHTML={{ __html: data.heroShortDesc }} />
           </div>
-          <div className="relative h-[30vh] 2xl:h-[60vh] 2xl:w-[90vh] p-4 outline outline-[15px] outline-primary 2xl:rounded-ss-[10rem] 2xl:rounded-ee-[10rem] overflow-hidden">
+
+          <div className="mb-7 relative h-[30vh] 2xl:h-[60vh] w-full 2xl:w-[90vh] p-4 outline outline-[15px] outline-primary 2xl:rounded-ss-[10rem] 2xl:rounded-ee-[10rem] overflow-hidden">
             <Image src={data.heroImage} alt="Team working" fill />
           </div>
         </div>
@@ -27,7 +28,7 @@ const Page = () => {
         <section key={i} className="p-10">
           <h1 className="text-4xl font-bold mb-10 text-center text-primary">Meet our team in {team.location}</h1>
 
-          <div className="flex justify-around">
+          <div className="flex justify-around flex-col gap-4 2xl:flex-row">
             {team.people.map((person, i) => (
               <div key={i}>
                 <div className="relative h-[400px] w-[300px] rounded-lg overflow-hidden mb-5">
