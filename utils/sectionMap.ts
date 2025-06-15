24a7fba3d8f66beb573aed2dfe6ecea31c967a64
Abdelghani Adel <a.adel@rockettravelsystem.com>
2025-06-15 13:15:57 +0300

    SectionsMap

--- utils/sectionMap.ts
+++ utils/sectionMap.ts
@@ -10,6 +10,9 @@ import HomeServices from "@/components/pages/home/HomeServices";
 import HomeSustainability from "@/components/pages/home/HomeSustainability";
 import CTA from "@/components/pages/home/CTA";
 import CareerHero from "@/components/pages/career/CareerHero";
+import CareerBenefits from "@/components/pages/career/CareerBenefits";
+import CareerIntro from "@/components/pages/career/CareerIntro";
+import Positions from "@/components/pages/career/Positions";
 
 export const sectionMap: Record<string, ComponentType> = {
   1: HomeHero,
@@ -22,4 +25,7 @@ export const sectionMap: Record<string, ComponentType> = {
   8: HomeSustainability,
   9: CTA,
   10: CareerHero,
+  11: CareerBenefits,
+  12: CareerIntro,
+  13: Positions,
 };
